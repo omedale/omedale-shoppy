1ce3fe55a934e255eff7d7a16a35cc85aff1e347
omedale <omedale@gmail.com>
2019-07-08 16:35:22 +0100

    setup ant design and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import {
+  Button
+} from 'ant-design-vue'
+
 import App from './App.vue'
 import router from './router'
+import './global.scss'
 
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
