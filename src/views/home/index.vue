294111645fbfe0f40457d4301f3a34d497b4c282
omedale <omedale@gmail.com>
2019-07-09 14:28:48 +0100

    add cart, checkout and stripe checkout

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,13 @@
 <template>
-<div class="hello">
+<div>
+  <filter-component></filter-component>
   <product-list></product-list>
 </div>
 </template>
 
 <script>
 import ProductList from '@/components/product/ProductList'
+import Filter from '@/components/common/Filter'
 export default {
   name: 'Home',
   data () {
@@ -14,7 +16,8 @@ export default {
     }
   },
   components: {
-    'product-list': ProductList
+    'product-list': ProductList,
+    'filter-component': Filter
   }
 }
 </script>
